19fe882b2e6e6175b1dc413280c91c6d00a9c98c
MaryDias <166772912+LabDevOps09@users.noreply.github.com>
2025-06-21 16:55:41 -0300

    Create causaeefeito.py
    add novo código Causa e efeito nada acontece por acaso (rs)

---
+++ causaeefeito.py
@@ -0,0 +1,6 @@
+# Ideia: Uma mudança em massa afeta diretamente a pressão (PV=nRT? (rs)).
+massa = 100  # kg
+area = 10  # m²
+
+pressao = massa * 9.8 / area
+print(f"A massa causou uma pressão de {pressao} Pa")
